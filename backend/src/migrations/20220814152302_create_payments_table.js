d2836ef6801857bbbff0942149121670b26042b4
Quyum Kehinde <quyumkehinde@gmail.com>
2022-08-14 16:36:26 +0100

    fix: updated payments schema

--- backend/src/migrations/20220814152302_create_payments_table.js
+++ backend/src/migrations/20220814152302_create_payments_table.js
@@ -1,4 +1,4 @@
-import { PAYMENT_METHODS } from '../utils/constants.js';
+import { PAYMENT_METHODS, PAYMENT_STATUSES } from '../utils/constants.js';
 
 /**
  * @param { import("knex").Knex } knex
@@ -7,15 +7,15 @@ import { PAYMENT_METHODS } from '../utils/constants.js';
 export function up(knex) {
     return knex.schema.createTable('payments', function (t) {
         t.bigIncrements('id').unsigned().primary();
-        t.bigInteger('user_id')
+        t.bigInteger('order_id')
             .unsigned()
             .notNullable()
             .references('id')
-            .inTable('users')
+            .inTable('orders')
             .onDelete('cascade');
         t.decimal('amount', 19, 4).notNullable();
         t.enum('method', PAYMENT_METHODS).notNullable();
-        t.enum('status', ['paymentProcessor', 'transfer']).notNullable();
+        t.enum('status', PAYMENT_STATUSES).notNullable();
         t.timestamps(true, true);
     });
 };
