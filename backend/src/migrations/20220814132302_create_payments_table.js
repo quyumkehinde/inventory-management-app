3e3307e90146286fcf898f425de2f33841a3a6aa
Quyum Kehinde <quyumkehinde@gmail.com>
2022-08-14 16:57:59 +0100

    refactor: payments and orders table

--- backend/src/migrations/20220814132302_create_payments_table.js
+++ backend/src/migrations/20220814132302_create_payments_table.js
@@ -7,12 +7,6 @@ import { PAYMENT_METHODS, PAYMENT_STATUSES } from '../utils/constants.js';
 export function up(knex) {
     return knex.schema.createTable('payments', function (t) {
         t.bigIncrements('id').unsigned().primary();
-        t.bigInteger('order_id')
-            .unsigned()
-            .notNullable()
-            .references('id')
-            .inTable('orders')
-            .onDelete('cascade');
         t.decimal('amount', 19, 4).notNullable();
         t.enum('method', PAYMENT_METHODS).notNullable();
         t.enum('status', PAYMENT_STATUSES).notNullable();
@@ -25,5 +19,5 @@ export function up(knex) {
  * @returns { Promise<void> }
  */
 export function down(knex) {
-  return knex.schema.dropTable('payments');
+    return knex.schema.dropTable('payments');
 };
--- backend/src/migrations/20220814145531_create_orders_table.js
+++ backend/src/migrations/20220814145531_create_orders_table.js
@@ -3,19 +3,22 @@
  * @returns { Promise<void> }
  */
 export function up(knex) {
-  return knex.schema.createTable('orders', function (t) {
+    return knex.schema.createTable('orders', function (t) {
         t.bigIncrements('id').unsigned().primary();
         t.decimal('amount', 19, 4).notNullable();
         t.bigInteger('user_id')
             .unsigned()
+            .nullable()
             .references('id')
             .inTable('users')
             .onDelete('set null');
         t.bigInteger('payment_id')
             .unsigned()
+            .nullable()
             .references('id')
             .inTable('payments')
             .onDelete('set null');
+        t.text('address').notNullable();
         t.timestamps(true, true);
     });
 };
