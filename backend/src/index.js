ebdcbf642ce1b02652ffe4d01e66c690004422cb
Quyum Kehinde <quyumkehinde@gmail.com>
2022-08-16 08:16:11 +0100

    fix: enabled cors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,13 @@
 import config from './config.js';
 import routes from './routes.js';
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cors())
 
 app.use('/api/v1/', routes);
 
